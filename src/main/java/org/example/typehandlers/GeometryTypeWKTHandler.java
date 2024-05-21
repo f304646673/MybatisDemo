bddf3ebd8fc580c025587fc7855eccc8f6cd524c
f304646673 <f304646673@gmail.com>
2024-05-21 11:20:03 +0800

    add wkt

---
+++ src/main/java/org/example/typehandlers/GeometryTypeWKTHandler.java
@@ -0,0 +1,62 @@
+package org.example.typehandlers;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+import org.locationtech.jts.io.WKTWriter;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GeometryTypeWKTHandler extends BaseTypeHandler<Geometry>  {
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Geometry parameter, JdbcType jdbcType) throws SQLException {
+        java.sql.Geometry
+        String str = serializeGeometry(parameter);
+        ps.setString(i, str);;
+    }
+
+    @Override
+    public Geometry getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        String str = rs.getString(columnName);
+        try {
+            return deserializeGeometry(str);
+        } catch (ParseException e) {
+            throw new SQLException(e);
+        }
+    }
+
+    @Override
+    public Geometry getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        String str = rs.getString(columnIndex);
+        try {
+            return deserializeGeometry(str);
+        } catch (ParseException e) {
+            throw new SQLException(e);
+        }
+    }
+
+    @Override
+    public Geometry getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String str = cs.getString(columnIndex);
+        try {
+            return deserializeGeometry(str);
+        } catch (ParseException e) {
+            throw new SQLException(e);
+        }
+    }
+
+    private static String serializeGeometry(Geometry geometry) {
+        WKTWriter writer = new WKTWriter(2);
+        return writer.write(geometry);
+    }
+
+    private static Geometry deserializeGeometry(String wkt) throws ParseException {
+        return new WKTReader().read(wkt);
+    }
+
+}
